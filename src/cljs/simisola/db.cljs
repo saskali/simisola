c2decf019fedbc7572fece2bca2d63a800b3c960
Saskia Lindner <saskia798@gmail.com>
2019-10-29 21:09:30 +0100

    Cleanup code

--- src/cljs/simisola/db.cljs
+++ src/cljs/simisola/db.cljs
@@ -6,39 +6,9 @@
 
 (def feelings #{:fatique :sad :tense})
 
-(def types #{:meditation :movement :magic :ritual :practice})
-
-(def feelings-needs-met #{:affectionate :engaged :hopeful :confident
-                          :excited :grateful :inspired :joyful
-                          :exhilarated :peaceful :refreshed})
-
-(def feelings-needs-not-met #{:afraid :annoyed :aversion :confused
-                              :disconnected :disquiet :embarrassed
-                              :fatigue :pain :sad :tense :vulnerable
-                              :yearning})
-
-(def meditations
-  [{:id 1
-     :time-span 20
-    :feelings #{:fatigue}
-    :content "URL1"}
-   {:id 2
-    :time-span 40
-    :feelings #{:sad}
-    :content "URL2"}
-   {:id 3
-    :time-span 20
-    :feelings #{:fatique}
-    :content "URL3"}
-   {:id 4
-    :time-span 20
-    :feelings #{:fatique :tense}
-    :content "URL4"}])
-
 (def default-db
   {:view        routes/time-span
    :time-spans  time-spans
    :feelings    feelings
-   :meditations meditations
    :state       {:time     nil
                  :feelings #{}}})
